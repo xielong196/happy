dcd4fdcd315c5525f32c4cde498773cf7eb2d1f5
xielong <xielong196@163.com>
2021-06-01 15:10:00 +0800

    news

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+       <router-link to="/news">News</router-link>
+  
     </div>
     <router-view/>
    
