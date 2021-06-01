fe9f2c90843de05f09070d85107713317b8f735d
xielong <xielong196@163.com>
2021-06-01 14:23:09 +0800

    dev2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      dev分子中的内容改动
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>dev分子中的内容改动</h3>
-   
+    HelloWorld页面
   </div>
 </template>
 
@@ -24,15 +19,5 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
